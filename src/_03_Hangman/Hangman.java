971669cfd1d8b1a31574789c44347bef29f381fe
league <league@192.168.1.214>
2026-08-29 11:28:08 -0700

    hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -13,31 +13,41 @@ import javax.swing.JPanel;
 public class Hangman implements KeyListener {
 
 	String currentWord;
-	StringBuilder x; 
+	JFrame frame = new JFrame();
+	Stack<String> words = new Stack<String>();
+	StringBuilder x;
 	JLabel label = new JLabel();
 	JLabel label2 = new JLabel("Lives: ");
-	JLabel incorrectGuesses = new JLabel("Incorrect Guesses: "); 
-	int lives = 5;
+	JLabel incorrectGuesses = new JLabel("Incorrect Guesses: ");
+	JLabel wordsSolvedLabel = new JLabel(); 
+	int wordsSolved = 0; 
+	int wordCount; 
+	int lives = 6;
+	boolean isPlaying = true;
 
 	public void run() {
-		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		frame.add(panel);
 		frame.addKeyListener(this);
-		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		panel.add(label);
 		panel.add(label2);
 		panel.add(incorrectGuesses);
+		panel.add(wordsSolvedLabel); 
 
 		frame.pack();
-
+		setNumberOfWords(); 
+		
+	}
+	
+	//sets the amount of words the user wants to play with
+	public void setNumberOfWords() {
 		String s = JOptionPane.showInputDialog(null,
 				"Enter the number of words you would like to use \n (Less than 266)");
-		int wordCount = Integer.parseInt(s);
+		wordCount = Integer.parseInt(s);
+		frame.setVisible(true);
 
-		Stack<String> words = new Stack<String>();
 		for (int i = 0; i < wordCount; i++) {
 			String temp = Utilities.readRandomLineFromFile("dictionary.txt");
 			if (!words.contains(temp)) {
@@ -46,41 +56,112 @@ public class Hangman implements KeyListener {
 				i--;
 			}
 		}
+		wordsSolvedLabel.setText(wordsSolved + "/" + wordCount);
+		setWord();
 
+		
+	}
+	
+	//sets the current word on the frame
+	public void setWord() {
+		label.setText("");
 		currentWord = words.pop();
-		x=new StringBuilder(currentWord); 
+		x = new StringBuilder(currentWord);
 		for (int i = 0; i < currentWord.length(); i++) {
 			label.setText(label.getText() + "_ ");
 
 		}
-		label.setText(label.getText() +currentWord);
+		label.setText(label.getText() + currentWord);
 
 		label2.setText(lives + "");
 		label2.setForeground(Color.RED);
 		frame.pack();
 
 	}
+	
+	//if they complete the game and win
+	public void gameCompleted() {
+		if(wordsSolved == wordCount) {
+			int playAgain = JOptionPane.showConfirmDialog(null,
+					"You won! Would you like to play again?", null,
+					JOptionPane.YES_NO_OPTION);
+			if (playAgain == JOptionPane.YES_OPTION) {
+				lives = 6;
+				incorrectGuesses.setText("Incorrect Guesses: ");
+				wordsSolved = 0; 
+				wordsSolvedLabel.setText(wordsSolved + "/" + wordCount); 
+//				if(!words.isEmpty()) {
+//					words.pop(); 
+//				}
+				setNumberOfWords();
+				setWord(); 
+			} else {
+
+			}
+		}
+	}
+	
+	public void gameLost() {
+		int playAgain = JOptionPane.showConfirmDialog(null,
+				"You lost. The correct answer was " + currentWord + ". \nWould you like to play again?", null,
+				JOptionPane.YES_NO_OPTION);
+		if (playAgain == JOptionPane.YES_OPTION) {
+			lives = 6;
+			incorrectGuesses.setText("");
+			wordsSolved = 0; 
+			wordsSolvedLabel.setText(wordsSolved + "/" + wordCount); 
+			setNumberOfWords();
+			setWord(); 
+		} else {
+
+		}
+		
+	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-//		if (currentWord.contains(e.getKeyChar() + "")) {
-//			StringBuilder labelText = new StringBuilder(label.getText());
-//			if(currentWord.charAt(currentWord.indexOf(e.getKeyChar())) == '_'){
-//				labelText.setCharAt(currentWord.indexOf(e.getKeyChar()) * 2, e.getKeyChar());
-//			}else if()
+
 		char key = e.getKeyChar();
-		if(x.indexOf(key + "") == -1) {
-			lives--; 
-			label2.setText(lives +"");
-			incorrectGuesses.setText(incorrectGuesses.getText() + key );
+		
+		//if they guess wrong
+		if (x.indexOf(key + "") == -1) {
+			lives--;
+			label2.setText(lives + "");
+			incorrectGuesses.setText(incorrectGuesses.getText() + key);
+			frame.pack();
 		}
+		
+		StringBuilder labelText = new StringBuilder(label.getText());
+		
+		//if they guess right
 		while (x.indexOf(key + "") >= 0) {
-			StringBuilder labelText = new StringBuilder(label.getText());
 			labelText.setCharAt(x.indexOf(key + "") * 2, key);
-			x.setCharAt(x.indexOf(key+""), '.');
+			x.setCharAt(x.indexOf(key + ""), '.');
 			label.setText(labelText.toString());
+			frame.pack();
 		}
+		
+		//if they solve a word that isn't the last word
+		if (labelText.indexOf("_") == -1 && wordsSolved<wordCount-1 && !words.isEmpty()) {
+			setWord();
+			wordsSolved++; 
+			wordsSolvedLabel.setText(wordsSolved + "/" + wordCount);
+		}
+		//if they solve the last word, calls gameCompleted
+		else if(labelText.indexOf("_") == -1 && wordsSolved==wordCount-1) {
+			wordsSolved++; 
+			wordsSolvedLabel.setText(wordsSolved + "/" + wordCount);
+			gameCompleted(); 
+		}
+		
+		
+		//if they run out of lives, calls gameLost
+		if (lives == 0) {
+			gameLost(); 
+		}
+		
+		
 
 	}
 
